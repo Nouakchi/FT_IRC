8193f6b7792cd19a02b78cd62632638f88832306
Kadigh53 <abdlkadraoutem@gmail.com>
2023-12-17 11:56:40 +0100

    ola

--- kitchen.cpp
+++ kitchen.cpp
@@ -1,36 +1,46 @@
-#include <iostream>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+// #include <iostream>
+// #include <stdlib.h>
+// #include <netinet/in.h>
+// #include <sys/socket.h>
+// #include <sys/types.h>
+// #include <netdb.h>
+// #include <arpa/inet.h>
 
-int main(int ac, char *av[]) {
+// int main(int ac, char *av[]) {
 
-	const char 	*addres;
-	char	ipstr[INET_ADDRSTRLEN];
-	struct addrinfo hints, *res, *p;
-	struct sockaddr_in *ipv4;
+// 	const char 	*addres;
+// 	char	ipstr[INET_ADDRSTRLEN];
+// 	struct addrinfo hints, *res, *p;
+// 	struct sockaddr_in *ipv4;
 	
-    int status;
-	void *addr ;
+//     int status;
+// 	void *addr ;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
-    hints.ai_socktype = SOCK_STREAM;
-	// hints.ai_flags = AI_PASSIVE;
+//     memset(&hints, 0, sizeof(hints));
+//     hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
+//     hints.ai_socktype = SOCK_STREAM;
+// 	// hints.ai_flags = AI_PASSIVE;
 
-    status = getaddrinfo(av[1], NULL, &hints, &res);
-	if (!res)
-		std::cout << "dadass\n";
-	for (p = res; p != NULL; p = p->ai_next)
-	{
-		ipv4 = (struct sockaddr_in *)p->ai_addr;
-		addr = &(ipv4->sin_addr);
-		addres = inet_ntop(p->ai_family, addr,ipstr, 100);
-		printf("\t%s\n", ipstr);
-	}
-	freeaddrinfo(res);
-	return 0;
+//     status = getaddrinfo(av[1], NULL, &hints, &res);
+// 	if (!res)
+// 		std::cout << "dadass\n";
+// 	for (p = res; p != NULL; p = p->ai_next)
+// 	{
+// 		ipv4 = (struct sockaddr_in *)p->ai_addr;
+// 		addr = &(ipv4->sin_addr);
+// 		addres = inet_ntop(p->ai_family, addr,ipstr, 100);
+// 		printf("\t%s\n", ipstr);
+// 	}
+// 	freeaddrinfo(res);
+// 	return 0;
+// }
+#include <iostream>
+void f()
+{
+	std::cout << "dd";
+}
+int main()
+{
+	int f;
+	f();
 }
--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 11:27:08 by aaoutem-          #+#    #+#             */
-/*   Updated: 2023/12/16 12:42:33 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2023/12/16 15:37:27 by aaoutem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,52 @@ void	setupServer()
 	int l;
 
 	memset(&hints,0,sizeof(hints));
-	hints.ai_family = AF_INET;
+	// hints.ai_family = AF_INET;
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	// hints.ai_flags = AI_PASSIVE;
+
 	int status = getaddrinfo("127.0.0.1", "6667", &hints, &res);
+
 	int sfd = socket(PF_INET, SOCK_STREAM, 0);
-	ServerAddress.sa_family = PF_INET;
-	l = listen(sfd, BACKLOG);
+
 	int b = bind(sfd, res->ai_addr, res->ai_addrlen);
-		while(true)
+
+	l = listen(sfd, BACKLOG);
+
+	while(true)
 	{
 		int accepted_fd = accept(sfd,res->ai_addr,&res->ai_addrlen);
 		if (accepted_fd != -1)
 		{
 			// handle the connection
+			// send() and recv()...
+			
+			// send section 
+			/*{
+				size_t msgsent_len;
+				char *msg = "message";
+				msgsent_len = send(accepted_fd, msg,sizeof(msg), 0);
+				// evaluate msglen if needed
+			}*/
+			// recieve section 
+			/*{
+				size_t msgreaded_len;
+				char buffer[MSG_LENGTH];
+				msgreaded_len = recv(accepted_fd, buffer, sizeof(buffer), 0);
+				if (msgreaded_len == 0)
+					// this means the client has closed the connection so handle it.
+			}*/
 		}
 		else
 		{/* error ;*/}
 	}
 	// int n = connect (sfd, &ServerAddress, sizeof(ServerAddress)); 
 }
+ void f(){}
+
+ int main()
+ {
+	int f;
+	f();
+ }
