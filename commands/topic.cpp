eed1ef9475e3f76d51f831b08eae5c9c1a0eeb8e
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2024-01-09 17:57:48 +0100

    fix topic

--- commands/topic.cpp
+++ commands/topic.cpp
@@ -6,7 +6,7 @@
 /*   By: heddahbi <heddahbi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 17:07:55 by heddahbi          #+#    #+#             */
-/*   Updated: 2024/01/07 17:07:56 by heddahbi         ###   ########.fr       */
+/*   Updated: 2024/01/09 17:46:33 by heddahbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,34 @@
 
 int ft_topicCmd(t_server *server, Client *clt, std::vector<std::string> &items)
 {
-    size_t size = items.size();
-    std::string ch = items[1];
-    if (size < 2)
-        return (
-            clt->reply(server->host_name, ERR_NEEDMOREPARAMS, "TOPIC :Not enough parameters"),
-            EXIT_FAILURE
-        );
-    if (!is_valid_channel_name(ch))
-        return (
-            clt->reply(server->host_name, ERR_BADCHANMASK, ch + " :Bad Channel Mask"),
-            EXIT_FAILURE
-        );
-    std::map<std::string, Channel*>::iterator it = server->channels.find(ch);
-    if (it == server->channels.end())
-        return (
-            clt->reply(server->host_name, ERR_NOSUCHCHANNEL, ch + " :No such channel"),
-            EXIT_FAILURE
-        );
-    std::map<std::string, Client *>::iterator is_opt = it->second->users.find("@" + clt->getNickName());
-    if (it->second->users.find(clt->getNickName()) == it->second->users.end() 
-        && is_opt == it->second->users.end())
-        return (
-            clt->reply(server->host_name, ERR_NOTONCHANNEL, ch + " :You're not on that channel"),
-            EXIT_FAILURE
-        );
+  size_t size = items.size();
+
+  if(size < 2)
+    return clt->reply(server->host_name, ERR_NEEDMOREPARAMS, "TOPIC :Not enough parameters"), EXIT_FAILURE; // not enough parameters
+    if(size > 2)
+    
+    if (server->channels.find(items[1]) == server->channels.end())
+        return clt->reply(server->host_name, ERR_NOSUCHCHANNEL, items[1] + " :No such channel"), EXIT_FAILURE; // no such channel
+    if (server->channels[items[1]]->users.find("@" + clt->getNickName()) == server->channels[items[1]]->users.end() && server->channels[items[1]]->users.find(clt->getNickName()) == server->channels[items[1]]->users.end())
+        return clt->reply(server->host_name, ERR_NOTONCHANNEL, items[1] + " :You're not on that channel"), EXIT_FAILURE; // you're not on that channel
     if (size == 2)
     {
-        if (it->second->topic == "")
-            return (
-                clt->reply(server->host_name, RPL_NOTOPIC, ch + " :No topic is set"),
-                EXIT_SUCCESS
-            );
-        return (
-            clt->reply(server->host_name, RPL_TOPIC, ch + " :" + it->second->topic),
-            EXIT_SUCCESS
-        );
+        std::string msg = ":" + clt->getNickName() + "!" + clt->getLoginName() + "@" + server->host_name + " TOPIC " + items[1] + " :" + server->channels[items[1]]->topic + " \r\n";
+        send(clt->getSocket(), msg.c_str(), msg.size(), 0);
     }
-    if (is_opt == it->second->users.end())
-        return (
-            clt->reply(server->host_name, ERR_CHANOPRIVSNEEDED, items[1] + " :You're not channel operator"),
-            EXIT_FAILURE
-        );
+    else
+    {
+        if (server->channels[items[1]]->users.find("@" + clt->getNickName()) == server->channels[items[1]]->users.end())
+            return clt->reply(server->host_name, ERR_CHANOPRIVSNEEDED, items[1] + " :You're not channel operator"), EXIT_FAILURE; // you're not channel operator
+        server->channels[items[1]]->topic = items[2];
+        std::map<std::string, Client *>::iterator u_it = server->channels[items[1]]->users.begin();
+        while (u_it != server->channels[items[1]]->users.end())
+        {
+            std::string msg = ":" + clt->getNickName() + "!" + clt->getLoginName() + "@" + server->host_name + " TOPIC " + items[1] + " :" + server->channels[items[1]]->topic + " \r\n";
+            std::cout << "-*- " << msg;
+            send(u_it->second->getSocket(), msg.c_str(), msg.size(), 0);
+            u_it++;
+        }
+    }
     return (EXIT_SUCCESS);
-
 }
